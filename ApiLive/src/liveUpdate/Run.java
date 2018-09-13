0f94b4784095b6e3ee7de9043dfda24165e73edb
PointBreak7 <42501398+PointBreak7@users.noreply.github.com>
2018-09-13 14:28:31 +0200

    Update Run.java

--- ApiLive/src/liveUpdate/Run.java
+++ ApiLive/src/liveUpdate/Run.java
@@ -14,28 +14,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Run {
-    //essential URL structure is built using constants
     //*declarations
     public static final String ACCESS_KEY="<Your_Access_Key_Here",
             BASE_URL = "http://apilayer.net/api/",
             ENDPOINT = "live";
-    
-    //this object is used for executing requests to the (REST) API
     static CloseableHttpClient httpClient = HttpClients.createDefault();
-    
-    /*
-    Notes:
-    
-    A JSON response of the form {"key":"rvalue"} is considered a simple Java JSONObject.
-    To get a simple value from the JSONObject, use: <JSONObject identifier>.get<type>("key");
-    
-    A json response of the form {"key":{"key":"value"}} is considered a complex Java JSONObject.
-    To get a complex value like another JSONObject, use: <JSONObject identifier>.getJSONObject("key");
-    
-    Values can also be JSONArray Objects. JSONArray objects are simple, consisting of multiple JSONObject Objects
-    
-    
-    */
+   
+    public static void main(String[] args) throws IOException {
+        sendLiveRequest();
+        httpClient.close();
+    }
     
     //sendLiveRequest() function is created to request and retrieve the data
     public static void sendLiveRequest(){
@@ -52,9 +40,9 @@ public class Run {
             
             System.out.println("Live Currency Exchange Rates");
             
-            //parsed JSON Objects are accessed according to the JSON resonse's hierarchy, output strings are built
-            
+            //the JSONObject that carries the data is turned to string and passed to a method to format the console printing
             printFormatedObect(exchangeRates.toString());
+            
             System.out.println("\n");
             response.close();
             
@@ -67,11 +55,6 @@ public class Run {
         } catch (JSONException e){
             e.printStackTrace();
         }
-    }
-    
-    public static void main(String[] args) throws IOException {
-        sendLiveRequest();
-        httpClient.close();
     }
     
     public static void printFormatedObect(String exchangeRate){
